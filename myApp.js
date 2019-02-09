cdf3b2dac132a6e9fad7e0ca1291248d43f70683
unknown <johngrhm@gmail.com>
2019-02-09 09:55:21 -0500

    finished express work

--- myApp.js
+++ myApp.js
@@ -1,12 +1,16 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
-
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /** 1) Meet the node console. */
 console.log("Hello World");
@@ -31,14 +35,25 @@ console.log("Hello World");
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.json({ 'time': req.time });
+});
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', (req, res) => {
+    res.json({ 'echo': req.params.word });
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name').get((req, res) => {
+    res.json({ 'name': `${req.query.first} ${req.query.last}` });
+}).post((req, res) => {
+    res.json({ 'name': `${req.body.first} ${req.body.last}` });
+});
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
